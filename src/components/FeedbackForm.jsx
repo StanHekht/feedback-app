da2fb801c3289d2a5638fef0fea91768c4610ee1
Stanislau Hekht <stanhekht@stanislaus-mbp.mynetworksettings.com>
2023-02-01 01:53:49 -0500

    Add feedback

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import Button from './shared/Button';
 import RatingSelect from './RatingSelect';
 import { useState } from 'react';
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
@@ -24,9 +24,23 @@ function FeedbackForm() {
     setText(e.target.value);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      handleAdd(newFeedback);
+      setText('');
+    }
+
+  }
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>
           How would you rate your service with us?
         </h2>

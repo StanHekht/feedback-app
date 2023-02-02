bed373e8f88377f97689334605854b860a9d2383
Stanislau Hekht <stanhekht@stanislaus-mbp.mynetworksettings.com>
2023-02-02 15:52:29 -0500

    Add editfeedback function to context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,19 +22,35 @@ export const FeedbackProvider = ({children}) => {
     }
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+
+  // Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   }
 
+  // Delete feedback
   const deleteFeedback = (id) => {
     setFeedback((feedback).filter((item) => item.id !== id));
+  }
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
   }
 
   return <FeedbackContext.Provider value = {{
     feedback,
     addFeedback,
     deleteFeedback,
+    editFeedback,
   }}>
     {children}
   </FeedbackContext.Provider>

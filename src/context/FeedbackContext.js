8257de3a49edaecdccabc44350824c6743b4a50f
Stanislau Hekht <stanhekht@stanislaus-mbp.mynetworksettings.com>
2023-02-02 23:41:44 -0500

    Configure updating and deleting feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -37,14 +37,29 @@ export const FeedbackProvider = ({children}) => {
   }
 
   // Delete feedback
-  const deleteFeedback = (id) => {
-    setFeedback((feedback).filter((item) => item.id !== id));
+  const deleteFeedback = async (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, {
+        method: 'DELETE',
+      })
+      setFeedback((feedback).filter((item) => item.id !== id));
+    }
   }
 
   // Update item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id? {...item, ...updItem} : item))
+      feedback.map((item) => (item.id === id? {...item, ...data} : item))
     )
     setFeedbackEdit({
       item: {},
